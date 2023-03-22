144f2a0e8aba664e7d22b2050f0f9b0d14babc5e
conorlum <conorlum@gmail.com>
2023-03-21 17:43:50 -0700

    parser not working
    parser

--- Hearthstone/PowerParser.py
+++ Hearthstone/PowerParser.py
@@ -1,24 +1,45 @@
-import json
+import re
 
+class PowerParser:
+    def __init__(self):
+        self.turn_num = 0
+        self.board_states = []
 
-def full_entity_creation_parser(file):
-	entities = {}
-	creating = False
-	creatingId = ""
-	for line in file:
-		if "FULL_ENTITY - Creating" in line:
-			ID = line.split("Creating ID=")[1].split(" ")[0]
-			entities[ID] = {}
-			creating = True
-			creatingId = ID
-		if creating and "GameState.DebugPrintPower() -         tag=" in line:
-			tag = line.split("tag=")[1].split(" ")[0]
-			value = line.split("value=")[1].strip("\n")
-			entities[creatingId][tag] = value
+    def boardParse(self, log_file):
+        self.turn_num = 0
+        self.board_states = []
 
-	print(json.dumps(entities, indent=4, sort_keys=True))
+        with open(log_file, "r") as f:
+            for line in f:
+                if "BLOCK_START BlockType=TRIGGER Entity=" in line and "BlockType=TRIGGER Entity=GameEntity" not in line:
+                    # print(line)
+                    self.turn_num += 1
+                    board_state = {"turn": self.turn_num, "enemy_board": [], "player_board": []}
 
+                # Enemy board state
+                if "FULL_ENTITY - Updating" in line and "cardId=" in line and "playerId=1" in line:
+                    match = re.search(r"id=(\d+).*zone=(\w+).*cardId=(\w+).*attack=(\d+).*health=(\d+)", line)
+                    if match:
+                        entity_id = match.group(1)
+                        zone = match.group(2)
+                        card_id = match.group(3)
+                        attack = match.group(4)
+                        health = match.group(5)
+                        board_state["enemy_board"].append({"entity_id": entity_id, "name": card_id, "attack": attack, "health": health})
 
-if __name__ == "__main__":
-	file = open("Power.log", "r")
-	full_entity_creation_parser(file)
+                # Player board state
+                if "FULL_ENTITY - Updating" in line and "cardId=" in line and "playerId=2" in line:
+                    match = re.search(r"id=(\d+).*zone=(\w+).*cardId=(\w+).*attack=(\d+).*health=(\d+)", line)
+                    if match:
+                        entity_id = match.group(1)
+                        zone = match.group(2)
+                        card_id = match.group(3)
+                        attack = match.group(4)
+                        health = match.group(5)
+                        board_state["player_board"].append({"entity_id": entity_id, "name": card_id, "attack": attack, "health": health})
+
+                if "BLOCK_END" in line:
+                    # print(line)
+                    self.board_states.append(board_state)
+
+        return self.board_states
--- Hearthstone/main.py
+++ Hearthstone/main.py
@@ -1,16 +1,17 @@
-from BoardController import BoardController
-from Board import Board
-from Card import Card
+from PowerParser import PowerParser
 
-if __name__ == "__main__":
+def main():
+    parser = PowerParser()
+    board_states = parser.boardParse("Power Logs/202303141727Power.log")
+    print(board_states)
+    for state in board_states:
+        print(f"Turn {state['turn']}:")
+        print("Enemy board:")
+        for minion in state["enemy_board"]:
+            print(f"\t{minion['name']} - Attack: {minion['attack']} - Health: {minion['health']}")
+        print("Player board:")
+        for minion in state["player_board"]:
+            print(f"\t{minion['name']} - Attack: {minion['attack']} - Health: {minion['health']}")
 
-	b1 = Board()
-	b2 = Board()
-	b1.add_card(Card(2,1, reborn = True),0)
-	b1.add_card(Card(1,1),1)
-	b2.add_card(Card(1,1),0)
-	b2.add_card(Card(1,1),2)
-	b2.add_card(Card(1,1),1)
-	BC = BoardController(b1, b2)
-	BC.simulate()
-	BC.board_states()
+if __name__ == '__main__':
+    main()
